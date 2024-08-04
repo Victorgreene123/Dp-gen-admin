7ec0a743deb55060f4ec028414dd35c55cf4688b
Simeon124 <chinedu.simeon2020@gmail.com>
2024-08-04 12:54:42 +0100

    removed irrelevant route

--- src/router/index.js
+++ src/router/index.js
@@ -1,7 +1,5 @@
 import DashBoard from '@/views/DashBoard.vue'
 import LogIn from '@/views/LogIn.vue'
-import ForgotPassword from '@/views/ForgotPassword.vue'
-import ResetPassword from '@/views/ResetPassword.vue'
 import ManageDP from '@/views/ManageDP.vue'
 import ManageAdmin from '@/views/ManageAdmin.vue'
 import AddAdmin from '@/views/AddAdmin.vue'
@@ -10,13 +8,31 @@ import { createRouter, createWebHistory } from 'vue-router'
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
-    { path: '/', name: 'Login', component: LogIn },
-    { path: '/forgot-password', name: 'ForgotPassword', component: ForgotPassword },
-    { path: '/reset-password/:token', name: 'ResetPassword', component: ResetPassword },
-    { path: '/dashboard', component: DashBoard },
-    { path: '/manage-dps', name: 'manage-dps', component: ManageDP },
-    { path: '/manage-admins', name: 'manage-admins', component: ManageAdmin },
-    { path: '/manage-admins/add-admin', name: 'add-admin', component: AddAdmin }
+    {
+      path: '/',
+      name: 'Login',
+      component: LogIn
+    },
+    {
+      path: '/dashboard',
+      name: 'Dashboard',
+      component: DashBoard
+    },
+    {
+      path: '/manage-dps',
+      name: 'manage-dps',
+      component: ManageDP
+    },
+    {
+      path: '/manage-admins',
+      name: 'manage-admins',
+      component: ManageAdmin
+    },
+    {
+      path: '/manage-admins/add-admin',
+      name: 'add-admin',
+      component: AddAdmin
+    }
   ]
 })
 
