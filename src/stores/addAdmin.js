57085f115c90c6655ab81ad1bbcccc48a83db836
Simeon124 <chinedu.simeon2020@gmail.com>
2024-08-05 11:27:09 +0100

    add admin functionality

---
+++ src/stores/addAdmin.js
@@ -0,0 +1,64 @@
+import axios from 'axios'
+import { onMounted, reactive, ref } from 'vue'
+
+const adminData = reactive({
+  message: '',
+  error: '',
+  isAdded: false
+})
+
+const email = ref('')
+const password = ref('')
+const fullname = ref('')
+const role = ref('Editor')
+
+const addAdmin = async () => {
+  try {
+    const { data } = await axios.put('https://achilles-web-be.onrender.com/admin/add', {
+      email: email.value,
+      password: password.value,
+      role: role.value,
+      fullname: fullname.value
+    })
+    console.log(data)
+    adminData.message = data
+    adminData.isAdded = true
+  } catch (error) {
+    adminData.error = error
+    adminData.isAdded = false
+
+    adminInnerText.value = 'Add Admin'
+    disableBtn.value = false
+  }
+  return adminData
+}
+
+// disable button and change text when adding admin
+const adminInnerText = ref('Add Admin')
+const disableBtn = ref(false)
+
+const isAddAdmin = () => {
+  adminInnerText.value = 'Adding Admin...'
+  setTimeout(() => {
+    disableBtn.value = true
+  }, 10)
+}
+
+// setup onMounted hook
+const setupAddAdmin = () => {
+  onMounted(async () => {
+    await addAdmin()
+  })
+}
+
+export {
+  setupAddAdmin,
+  addAdmin,
+  adminData,
+  email,
+  password,
+  fullname,
+  isAddAdmin,
+  adminInnerText,
+  disableBtn,
+}
